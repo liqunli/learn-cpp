b534814dbe759304cf4df3b0b69b15a59e0beb64
liqunli <leland.liqun.li@gmail.com>
2013-04-25 23:16:34 +0800

    add comments to qsort.h

--- Sorting/qsort/qsort.h
+++ Sorting/qsort/qsort.h
@@ -1,7 +1,7 @@
 #ifndef QSORT_H
 #define QSORT_H
 
-//swap two elements
+//swap two elements in an array
 void swap(int* array, int i, int j)
 {
 	int tmp = array[i];
@@ -12,28 +12,33 @@ void swap(int* array, int i, int j)
 //the function has two index parameters left and right which are used for resursive calling
 void my_qsort(int* array, int left, int right)
 {
+	//there is always an ending condition for recursive 
 	if(left >= right)
 	{
 		return;
 	}
 	int i = left + 1;
 	int j = left + 1;
-	//the object is to detect if array[i] > array[j] where i < j
-	//we pick the first element as the divider
-	int judge = array[left];
+
+	//We pick the first element as the pivot. It is free to use any element as pivot.
+	//An improvement can be made by first selecting a random element in the array and 
+	//swapping it with the first one.
+	int pivot = array[left];
 	while(true)
 	{
-		//find the first element larger than judege
-		while(i < right && array[i] <= judge)
+		//find the first element larger than pivot 
+		while(i < right && array[i] <= pivot)
 		{
 			i++;
 		}
+		//find the first element smaller than pivot after i
 		j = i + 1;
-		//find the first element smaller than judge after i
-		while(j < right && array[j] > judge)
+		while(j < right && array[j] > pivot)
 		{
 			j++;
 		}
+		// if i < right && j < right, we need to swap the ith and jth element to 
+		// ensure that all left elements are <= pivot, while those on the right > pivot
 		if(i < right && j < right)
 		{
 			swap(array, i, j);
@@ -41,10 +46,16 @@ void my_qsort(int* array, int left, int right)
 		}
 		else
 		{
+			//this is a bit tricky. i >= right means that all elements are <= pivot.
+			//so we should swap pivot with the last element, i.e., the (i-1)th. 
+			//If j >= right, it means that all elements after i are > pivot including i.
+			//so we also should swap pivot with the (i-1)th element.
 			swap(array, i-1, left);
+			//we break here as the pivot arrives the right position in the array.
 			break;
 		}
 	}
+	//deal with the left/right sub-problems
 	my_qsort(array, left, i - 2);
 	my_qsort(array, i, right);
 }
